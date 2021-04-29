df51600fcdf0c8e5d199c240194ac4366dfeddc0
rynptr <rianputra2302@gmail.com>
2021-04-29 11:16:43 +0700

    r

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    jwt = require('jsonwebtoken'),
+    app = express(),
+    port = process.env.PORT,
+    bodyParser = require('body-parser'),
+    controller = require('./controller');
+
+
+var cors = require('cors');
+app.use(cors());   
+
+
+// set tampilan mesin untuk nodejs
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
+    next();
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./routes');
+routes(app);
+
+app.listen(port, () => {
+    console.log('Server is running');
+});
